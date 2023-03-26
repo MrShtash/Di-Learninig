438afc8725780135affbc5a36205553ae1f9cbff
Stanislav <bassketb86@gmail.com>
2023-03-26 14:02:08 +0300

    add python XP ex

---
+++ Week2/Day1/Exercise_XP/exXP.py
@@ -0,0 +1,45 @@
+#for i in range(4):
+#    print('Hello world')
+
+#print((99**3)*8)
+
+#print(5 < 3)
+#print(3 == 3)
+#print(3 == "3")
+#print("3" > 3)
+#print("Hello" == "hello")
+
+#name = "MacBook"
+#sentence = f"I have a {name} computer"
+#print(sentence)
+
+#name = "St"
+#age = "36"
+#shoe_size = "10UK"
+#info = f"My name is {name}, I'm {age} years old, my shoes is {shoe_size}"
+#print(info)
+
+#a = 10
+#b = 5
+
+#if a > b:
+#    print('Hello world')
+
+#num = input('enter your number')
+#if int(num) % 2 == 0:
+#    print('even')
+#else:
+#    print('od')
+
+#num = input('enter your name')
+#if num == 'Stas':
+#    print('wow, take a cake')
+#else:
+#    print('Stas is better')
+
+num = input('enter your height in inches')
+a = 145
+if int(num) > a:
+    print('you can ride')
+else:
+    print('you need to grow')
