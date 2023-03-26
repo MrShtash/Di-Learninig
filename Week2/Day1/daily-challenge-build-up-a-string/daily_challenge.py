1547d7edbe77f3b33f08d6c95bacf610cf23f018
Stanislav <bassketb86@gmail.com>
2023-03-26 15:50:35 +0300

    add daily challenge

---
+++ Week2/Day1/daily-challenge-build-up-a-string/daily_challenge.py
@@ -0,0 +1,25 @@
+#num = input('enter your string')
+#lengh = len(num)
+#print(lengh)
+
+#if lengh > 10:
+#    print('string too long')
+#if lengh < 10:
+#    print('string not long enough')
+#else:
+#    print(f'{num[0]} , {num[-1]}')
+
+#num = input('enter text')
+#print(num)
+#m=""
+#for i in num:
+#    m+=i
+#    print(m)
+
+import random
+x = list('developer')
+random.shuffle(x)
+result = ''.join(x)
+print(result)
+
+
