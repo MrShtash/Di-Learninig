6a0a4dae1d397e88c5fa067b098f242756a399b6
Stanislav <bassketb86@gmail.com>
2023-03-28 00:51:26 +0300

    add daily challenge

---
+++ Week2/Day2/Daily-challenge/daily-challenge.py
@@ -0,0 +1,24 @@
+# First
+# result = []
+# number=int(input("Type a number: "))
+# length=int(input("Type a multiplier: "))
+# a = 0
+# b = int(a)
+
+# while b < length:
+#   result.append(number * (b+1))
+#   b = b + 1
+# print(result)
+
+# Second
+# text=input("write a text: ")
+# list=list(text)
+# i = 0
+
+# while i < len(list) - 1:
+#     if list[i] == list[i+1]:
+#         del list[i]
+#     else:
+#         i = i + 1
+
+# print(list)
