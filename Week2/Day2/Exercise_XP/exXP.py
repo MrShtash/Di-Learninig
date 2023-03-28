c28d3601fccf5481f0eefefea7d28d003699d0a8
Stanislav <bassketb86@gmail.com>
2023-03-28 08:09:58 +0300

    del incorrect inf in my file

--- Week2/Day2/Exercise_XP/exXP.py
+++ Week2/Day2/Exercise_XP/exXP.py
@@ -140,10 +140,6 @@
 # Sandwich_2
 sandwich_orders = ["Tuna sandwich", "Avocado sandwich", "Egg sandwich", "Sabih sandwich", "Pastrami sandwich"]
 print('The deli has run out of pastrami')
-while "Pastrami sandwich" in sandwich_orders:
-  sandwich_orders.remove("Pastrami sandwich")
-print(sandwich_orders)
-
 del_sandwich="Pastrami sandwich"
 while del_sandwich in sandwich_orders:
     sandwich_orders.remove(del_sandwich)
