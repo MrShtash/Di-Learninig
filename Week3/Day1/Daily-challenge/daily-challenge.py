0fd340b05f05dde46d25e011a80892ff86db2a2b
Stanislav <bassketb86@gmail.com>
2023-04-02 17:46:44 +0300

    correct

--- Week3/Day1/Daily-challenge/daily-challenge.py
+++ Week3/Day1/Daily-challenge/daily-challenge.py
@@ -15,8 +15,21 @@ class Farm:
     def get_animal_types(self):
         return sorted(list(self.animals.keys()))
 
-    def get_info(self):
-        print(f'McDonald’s farm has {macdonald.get_animal_types()}')
+    # def get_info(self):
+    #     print(f'McDonald’s farm has {macdonald.get_animal_types()}')
+        
+    def get_short_info(self):
+        m=self.get_animal_types()
+        return (f"McDonald's farm has {m[0]}s, {m[1]}s and {m[2]}")
+    
+    def get_res_info(self):
+        m=self.get_animal_types()
+        return (f'''McDonald's farm:
+{m[0]}  :  5
+{m[1]}  :  2
+{m[2]}  :  12
+    E-I-E-I-0!
+    ''')
 
 macdonald = Farm('McDonald')
 macdonald.add_animal('cow', 5)
@@ -25,6 +38,6 @@ macdonald.add_animal('sheep')
 macdonald.add_animal('goat', 12)
 print(macdonald.animals)
 print(macdonald.get_animal_types())
-print(macdonald.get_info())
-
-
+# print(macdonald.get_info())
+print(macdonald.get_short_info())
+print(macdonald.get_res_info())
