9a9dd7d2a8e90389d29204cead3ae5e784937214
Stanislav <bassketb86@gmail.com>
2023-04-04 22:36:34 +0300

    correct circle list

--- Week3/Day3/Daily-challenge-circle/daily_challenge_circle.py
+++ Week3/Day3/Daily-challenge-circle/daily_challenge_circle.py
@@ -15,11 +15,12 @@ circle7 = Circle(7)
 circle_sum_r = circle5.radius + circle6.radius
 circle_dif_r = circle5.radius > circle6.radius
 circle_eq_r = circle5.radius == circle6.radius
-circle_list = [circle7.radius, circle5.radius, circle6.radius]
-circle_list.sort()
-print(circle_list)
-
-
+circle_list = [circle7, circle5, circle6] 
+circle_list = sorted(circle_list, key=lambda x: x.radius) # sort list
+# sorted(circle_list, key=lambda x: x.radius)
+for x in circle_list: # sort list and take only radius
+     print(x.radius)
+     
 print(circle5.name)
 print(circle5.radius)
 print(circle6.radius)
