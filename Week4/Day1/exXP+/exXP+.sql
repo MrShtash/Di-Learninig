761fea52e8fa4edf2524a2986b5ef7d77e375927
Stanislav <bassketb86@gmail.com>
2023-04-16 14:16:10 +0300

    add xp+

---
+++ Week4/Day1/exXP+/exXP+.sql
@@ -0,0 +1,41 @@
+-- create table students (
+-- id serial primary key,
+-- f_name varchar(20),
+-- l_name varchar(20),
+-- birth_date date
+-- );
+
+-- insert into students (f_name, l_name, birth_date) values
+-- ('Mark', 'Benichou', '1998-11-02'),
+-- ('Yoan', 'Cohen', '2010-12-03'),
+-- ('Lea', 'Benichou', '1987-07-27'),
+-- ('Amelia', 'Dux', '1996-04-07'),
+-- ('David', 'Grez', '2003-06-14'),
+-- ('Omer', 'Simpson', '1990-10-03');
+
+-- insert into students (f_name, l_name, birth_date) values
+-- ('Stas', 'Tuf', '1986-08-18');
+
+-- select * from students;
+
+-- select f_name, l_name from students;
+
+-- select f_name, l_name from students where id = 2;
+
+-- select * from students where l_name = 'Benichou' or f_name = 'Mark';
+
+-- select * from students where f_name similar to '%a%';
+
+-- select * from students where f_name similar to 'A%';
+
+-- select * from students where f_name similar to '%a';
+
+-- select * from students where f_name similar to '%a_';
+
+-- select * from students where id = 1 or id = 3;
+
+-- select * from students where birth_date <= '2020-01-01';
+
+
+
+
