bf77cb83467d562a0f353b175d8d14aca3645218
Stanislav <bassketb86@gmail.com>
2023-04-16 14:49:14 +0300

    add d-ch

---
+++ Week4/Day1/daily_challenge/daily_challenge.sql
@@ -0,0 +1,28 @@
+-- create table actors (
+-- actor_id serial primary key,
+-- first_name varchar (50) NOT NULL,
+-- last_name varchar (100) NOT NULL,
+-- age date NOT NULL,
+-- number_oscars smallint NOT NULL
+-- );
+
+-- insert into actors (first_name, last_name, age, number_oscars) values
+-- ('Matt','Damon','08/10/1970', 5),
+-- ('George','Clooney','06/05/1961', 2);
+
+-- insert into actors (first_name, last_name, age, number_oscars) values
+-- ('Jennifer','Lawrence','08/15/1992', 4);
+-- delete from actors where actor_id = 3;
+
+-- insert into actors (first_name, last_name, age, number_oscars) values
+-- ('Angelina','Jolie','06/04/1975', 3);
+
+-- insert into actors (first_name, last_name, age, number_oscars) values
+-- ('Al','Pacino','04/25/1940', 8),
+-- ('Robert','De Niro','08/17/1943', 8),
+-- ('Leonardo','DiCaprio','11/11/1974', 3);
+
+-- select count(*) from actors;
+
+-- insert into actors (first_name, last_name, age, number_oscars) values
+-- (' ',' ', ' ', ); - NOT WORKING
