0e6c49f5d08637d7668c7f542c5b063dae05fe38
Stanislav <bassketb86@gmail.com>
2023-04-16 13:39:31 +0300

    add exXP

---
+++ Week4/Day1/exXP/public.sql
@@ -0,0 +1,37 @@
+-- create table items (
+-- id serial primary key,
+-- size varchar(20),
+-- price float
+-- )
+
+-- insert into items (size, price) values
+-- ('Small Desk', 100),
+-- ('Large Desk', 300),
+-- ('Fan', 80);
+
+
+-- create table customers (
+-- id serial primary key,
+-- f_name varchar(20),
+-- l_name varchar(20)
+-- );
+
+-- insert into customers (f_name, l_name) values
+-- ('Greg', 'Jones'),
+-- ('Sandra', 'Scott'),
+-- ('Trevor', 'Green'),
+-- ('Melanie', 'Johnson');
+
+-- select * from items, customers;
+-- select * from items;
+-- select * from customers;
+
+-- select size from items;
+-- select size from items where price > 80; 
+-- select * from items where price < 300;
+
+-- select * from customers where l_name = 'Smith';
+-- select * from customers where l_name = 'Jones';
+-- select * from customers where f_name != 'Scott';
+
+
