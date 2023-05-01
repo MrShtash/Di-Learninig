6aa44746c5eacb853c0b750b4b374ba423eef022
Stanislav <bassketb86@gmail.com>
2023-05-01 14:06:40 +0300

    fix urls

--- Week6/Day1/exXP/imdi/FilmProject/FilmProject/urls.py
+++ Week6/Day1/exXP/imdi/FilmProject/FilmProject/urls.py
@@ -5,6 +5,6 @@ from films.views import *
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('films/homepage/', home),
-    path('add-film/', FilmCreateView.as_view(), name='addfilm'),
-    path('add-director/', DirectorCreateView.as_view(), name='adddirector'),
+    path('films/add-film/', FilmCreateView.as_view(), name='addfilm'),
+    path('films/add-director/', DirectorCreateView.as_view(), name='adddirector'),
 ]
