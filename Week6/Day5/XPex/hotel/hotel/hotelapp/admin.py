47b21fd510782d7e52ce74b8b3709147c0290a22
Stanislav <bassketb86@gmail.com>
2023-05-15 20:56:02 +0300

    add view

--- Week6/Day5/XPex/hotel/hotel/hotelapp/admin.py
+++ Week6/Day5/XPex/hotel/hotel/hotelapp/admin.py
@@ -5,3 +5,7 @@ from .models import *
 
 admin.site.register(Type)
 admin.site.register(Size)
+admin.site.register(Room)
+admin.site.register(Review)
+admin.site.register(Booking)
+admin.site.register(ApartmentPhoto)
