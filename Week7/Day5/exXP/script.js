ebfa07ab4a626fc4f5834353ea97f7e88d19b0de
Stanislav <bassketb86@gmail.com>
2023-05-18 12:28:29 +0300

    fix and fin

--- Week7/Day5/exXP/script.js
+++ Week7/Day5/exXP/script.js
@@ -1,3 +1,4 @@
+let count = 0;
 function playTheGame() {
     let num;
     if (confirm('Do u want 2 play Game?')) {
@@ -11,13 +12,25 @@ function playTheGame() {
                 alert ('sorry, it is not a number in range from 0 to 10, byyyye');
                 return;
             }
+            else{
+                let comp_num = Math.floor(Math.random()*10);
+                console.log(comp_num);
+                let res = compareNumbers(num, comp_num);
+                if (res == true) {
+                    return
+                } 
+            }
         }
-        while (isNaN(num) || num < 0 || num > 10);
-        let res = num;
-        let comp_num = Math.floor(Math.random()*10);
-        console.log(comp_num);
-        compareNumbers(num,comp_num);
-        return res;   
+        while (count<3)
+        if(count >= 3){
+            alert ('out of chance')
+        }
+        // while (isNaN(num) || num < 0 || num > 10);
+        // let res = num;
+        // let comp_num = Math.floor(Math.random()*10);
+        // console.log(comp_num);
+        // compareNumbers(num,comp_num);
+        // return res;   
     }
     else {
         alert ('ok. no pb, byyyye'); 
@@ -43,17 +56,19 @@ function playTheGame() {
 //     }
 // }
 
-function compareNumbers(userNumber,computerNumber) {
-    if (userNumber == computerNumber) {
-        alert ('YOU WIN!');
-        return
-    }
-    else if (userNumber > computerNumber) {
-        alert('your number is bigger than computer number, try again')
-        return playTheGame();
-    }
-    else {
-        alert('your number is bigger than computer number, try again')
-        return playTheGame();
-    }
+function compareNumbers(userNumber, computerNumber) {
+        if (userNumber == computerNumber) {
+            alert ('YOU WIN!');
+            return true;
+        }
+        else if (userNumber > computerNumber) {
+            alert('your number is bigger than computer number, try again');
+            count++
+            return false
+        }
+        else {
+            alert('your number is bigger than computer number, try again');
+            count++
+            return false
+        }
 }
