984f3e72af8539c57622f6c9fcc35d31cf182c28
Stanislav <bassketb86@gmail.com>
2023-05-23 09:27:51 +0300

    fix

--- Week7/Day5/Daily/script.js
+++ Week7/Day5/Daily/script.js
@@ -8,18 +8,18 @@ function bottlesOnTheWall() {
     );
     if (isNaN(beer)) {
       alert("sorry, its not a number, enter number from 0 to 99");
-    } else if (beer < 0 || beer > 99) {
+    } else if (beer < 0 || beer > bottles) {
       alert(
         "sorry, it is not a number in range from 0 to 99, enter number from 0 to 99"
       );
-    } else if (beer == 1) {
-      bottles = bottles - beer;
+    } else if (beer === 1) {
+      bottles -= beer;
       console.log(`Take ${beer} down, pass it around`);
       console.log(`we have now ${bottles} bottles`);
       // return res;
     }
     else {
-      bottles = bottles - beer;
+      bottles -= beer;
       console.log(`Take ${beer} down, pass them around`);
       console.log(`we have now ${bottles} bottles`);
       // return res;
