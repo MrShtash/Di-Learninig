ffbc784a9082d89afa57c61a68cc6c5fd63991fb
Stanislav <bassketb86@gmail.com>
2023-05-24 14:59:47 +0300

    fin Gold

---
+++ Week8/Day4/exXP_Gold/script.js
@@ -0,0 +1,52 @@
+// 1
+// const studentObj = {
+//     first: 'Elie',
+//     last:'Schoppik',
+// }
+// function printFullName({first, last}) {
+//     console.log(`Your full name is ${first} ${last}`);
+// }
+// printFullName(studentObj)
+
+
+
+
+
+// 2 Keys and Values
+// let keysAndValues = {a: 1, b: 2, c: 3};
+// let arr = [Object.keys(keysAndValues), Object.values(keysAndValues)]
+// console.log(arr);
+
+// let keysAndValues = {a: "Apple", b: "Microsoft", c: "Google"};
+// let arr = [Object.keys(keysAndValues).sort(), Object.values(keysAndValues).sort()]
+// console.log(arr);
+
+// let keysAndValues = {key1: true, key2: false, key3: undefined};
+// let arr = [Object.keys(keysAndValues).sort(), Object.values(keysAndValues)]
+// console.log(arr);
+
+
+
+
+
+// 3 Counter class
+class Counter {
+  constructor() {
+    this.count = 0;
+  }
+
+  increment() {
+    this.count++;
+  }
+}
+
+const counterOne = new Counter();
+counterOne.increment();
+counterOne.increment();
+
+const counterTwo = counterOne;
+counterTwo.increment();
+
+console.log(counterOne.count);
+// forcast: 2, we create counterOne+1, after that 1+1=2 (line 45) and create counterTwo = counterOne
+// fact: 3
