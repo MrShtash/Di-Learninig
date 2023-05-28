ce898a936dd625fb5dce26ed09ac42ab7fb91a0d
Stanislav <bassketb86@gmail.com>
2023-05-28 22:03:55 +0300

    fix

--- Week9/Day1/miniProject/script.js
+++ Week9/Day1/miniProject/script.js
@@ -26,8 +26,6 @@ const quoteArr = [
 ];
 // console.log(quoteArr[0]);
 
-let form1 = document.forms.myForm;
-
 const quoteP = document.getElementById("quote");
 const authorP = document.getElementById("author");
 
@@ -80,30 +78,29 @@ function calcLSpace(){
   const qLength = qText.length;
   // alert(`This quote has ${lQuoteId.length} characters`)
   alert(`This quote has ${qLength} characters`);
-
 }
 
 btnwthts = document.getElementById('wthts');
 btnwthts.addEventListener('click', calcLengthNoSpace);
 
 function calcLengthNoSpace(){
-  const currentQuote = quoteArr[lQuoteId];
-  const qText = currentQuote.quote;
-  const qLength = qText.trim().length;
-  alert(`This quote has ${qLength.length} characters`)
+  // const currentQuote = quoteArr[lQuoteId];
+  // const qText = currentQuote.quote;
+  // const qLength = qText.trim().length;
+  // alert(`This quote has ${qLength.length} characters`)
+  const currentQuote = quoteArr[lQuoteId].quote;
+  const trimmedText = currentQuote.split(" ").join("");
+  const qLength = trimmedText.length;
+  console.log(qLength);
+  console.log(trimmedText);
+  alert(`This quote has ${qLength} characters`);
 }
 
 btnw = document.getElementById('w');
 btnw.addEventListener('click', calcWords);
 
 function calcWords(){
-  let words = 0;
-  for (char of lQuoteId.innerText) {
-    if (char == ' ') {
-        words++
-    }
-  }
-  alert(`This quote has ${words} words`)
+    alert(`This quote has ${myLength.length} words`);
 }
 
 btnl = document.getElementById('l');
@@ -114,19 +111,62 @@ function calcLike(){
     alert(`This quote has ${quoteArr[lQuoteId].likes} likes`)
 }
 
-function addQuote (event) {
-  let form = document.forms.addQuote;
+let form = document.getElementById("AddQuote");
+
+function addQuote(event) {
+  event.preventDefault();
   let nQ = {
     id: quoteArr.length,
     author: form.author.value,
     quote: form.quote.value,
     likes: 0,
-  }
-  event.preventDefault(),
-  quoteArr.push(nQ),
+  };
+  quoteArr.push(nQ);
   console.log(quoteArr);
-  alert(`${nQ}`)
+  alert(`${nQ.quote}`);
+}
+form.addEventListener("submit", addQuote);
+
+let filterForm = document.getElementById("filterQuote");
+filterForm.addEventListener('submit', filterQuote)
+
+function search(event){
+  event.preventDefault();
+    let filtered = quoteArr.filter((item) => item.author.includes(filterForm.search.value))
+    if (filterForm.search.valuelength > 0){
+        for (item of window.children){
+          body.removeChild(item)
+        }
+       for (item of filtered){
+          let newQ = document.createElement('p')
+          newQ.innerText = item.quote + '\n' + item.author
+          body.appendChild(newQ)
+       }
+    }  
 }
 
-// addQ(event);
+// let currentIndex = 0;
+let prevButton = document.getElementById("prev");
+let nextButton = document.getElementById("next");
+prevButton.addEventListener("click", showPQuote);
+nextButton.addEventListener("click", showNQuote);
+
+function showPQuote() {
+  if (lQuoteId > 0) {
+    lQuoteId--;
+    displayQuote(filtered[lQuoteId]);
+  }
+}
+
+function showNQuote() {
+  if (lQuoteId < filtered.length - 1) {
+    lQuoteId++;
+    displayQuote(filtered[lQuoteId]);
+  }
+}
+
+function displayQuote(quote) {
+  quoteP.innerText = quote.quote;
+  authorP.innerText = quote.author;
+}
 
