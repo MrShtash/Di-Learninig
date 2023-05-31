e18cd36ab22ec63e4a20ad0a968387cd4a4b4d0d
Stanislav <bassketb86@gmail.com>
2023-05-31 14:14:12 +0300

    fix

--- Week9/Day4/exXP/script.js
+++ Week9/Day4/exXP/script.js
@@ -4,34 +4,50 @@ finder.addEventListener('click', submita);
 
 let loading = document.getElementById('loading');
 let icon = document.getElementById('icon');
+let homeworld;
+
+let contDiw = document.getElementById('content');
 
 async function submita(event)
 {
     event.preventDefault();
     showLoading()
     let id = Math.floor(Math.random()*100)
-    let r = await fetch(`https://www.swapi.tech/api/people/${id}`)
+    
         // .then(res => res.json())
         // .then(data => console.log(data))
         // .catch(err => console.error(err))
     try
     {
+        let r = await fetch(`https://www.swapi.tech/api/people/${id}`)
+        if(!r.status == '200'){
+            throw new Error('not found')
+        }
         const json = await r.json();
         hideLoading();
             // console.log(json.result.properties);
-        swResult(json.result.properties)
+        let homeworldname = 'not valid'
+        console.log(json.result.properties.homeworld);
+        if (json.result.properties.homeworld)
+        {
+            let r2 = await fetch(json.result.properties.homeworld)
+            let resFromR2 = await r2.json()
+            // homeworld = r2.result.properties.name
+            homeworldname = resFromR2.result.properties.name;
+        }
+        swResult({...json.result.properties,homeworld:homeworldname})
     }
     catch(e)
     // catch()
     {
         hideLoading();
+        contDiw.innerHTML = '';
         let erMessage = document.createElement('p');
         erMessage.textContent = "Oh No! That person isn't available.";
         erMessage.setAttribute('style', 'color: yellow');
         contDiw.appendChild(erMessage);
         console.log(`${e} "Oh No! That person isnt available.`);
         // console.log("Oh No! That person isnt available.")
-
     }
 }
 
@@ -45,7 +61,7 @@ function hideLoading() {
 
 function swResult(properties) {
     console.log(properties);
-    let contDiw = document.getElementById('content');
+    
     contDiw.innerHTML = '';
     const labels = {
        name: 'Name: ',
