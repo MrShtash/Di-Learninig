e0684efee817de7bfe0f34c183ef894057233388
Stanislav <bassketb86@gmail.com>
2023-06-06 14:43:36 +0300

    daily

---
+++ Week10/Day3/Daily/fs.js
@@ -0,0 +1,69 @@
+const fs = require("fs");
+const data = "RightLeft.txt";
+
+// 2
+// fs.readFile('RightLeft.txt','utf-8', (err, data) => {
+//     if(err){
+//         console.log(err);
+//     }
+//     else{
+//         console.log(data);
+//     }
+// });
+
+// 3-5
+let position = 0;
+let sum = 0;
+let checked = false;
+fs.readFile('RightLeft.txt', 'utf-8', (err, data) => {
+    for (item of data){
+        sum++
+        if (item == '>') {
+            position++
+        }
+        else if (item == '<') {
+            position--
+        }
+        if (position == -1 && checked == false) {
+            console.log(`Current position is ${position}. It took ${sum} steps to reach this`)
+            checked = true
+        }
+        }
+    console.log(`Final position at the end of the file: ${position}`);
+});
+
+
+
+
+
+
+
+
+// 3
+// >>>>><>><>>>>>>>><<<>>>><
+// 0123456789
+// >>>>><><<>><<><>>>><>>>>>>><
+
+// function calcFinal() {
+//     let stepCount = arr.from(data);
+//     let position = 0;
+//     let sum = 0;
+//     let step = 0;
+//     fs.readFile('RightLeft.txt', 'utf-8', (err, data) => {
+//         for(i=0; i<data.length; i++) {
+
+//         }
+//     })
+// }
+
+// let position = 0;
+// let sum = 0;
+// let step = 0;
+
+// function calcFinal() {
+//   fs.readFile("RightLeft.txt", "utf-8", (err, data) => {
+//     console.log(typeof data);
+//   });
+// }
+
+// calcFinal();
