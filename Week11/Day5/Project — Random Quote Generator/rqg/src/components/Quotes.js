763af64819f6bbca263e3486d78e64c8eaf8b33e
Stanislav <bassketb86@gmail.com>
2023-06-15 15:57:32 +0300

    add comments

--- Week11/Day5/Project — Random Quote Generator/rqg/src/components/Quotes.js
+++ Week11/Day5/Project — Random Quote Generator/rqg/src/components/Quotes.js
@@ -13,6 +13,7 @@ class Quotes extends React.Component {
     };
   }
 
+  // set an initial code when we open page
   componentDidMount() {
     const randomQuote = quotes[this.state.idx];
     this.setState({quote: randomQuote.quote, author: randomQuote.author});
