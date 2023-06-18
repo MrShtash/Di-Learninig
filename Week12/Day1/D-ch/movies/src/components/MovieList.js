085b1f4e2367330e38b0a109d06384d8501f665b
Stanislav <bassketb86@gmail.com>
2023-06-18 18:08:47 +0300

    fix

--- Week12/Day1/D-ch/movies/src/components/MovieList.js
+++ Week12/Day1/D-ch/movies/src/components/MovieList.js
@@ -4,11 +4,6 @@ import { movieInfo } from "../redux/actions";
 
 const Movie = (props) => {
   console.log(props.movies[0].title);
-  //   const titles = props.movies.map((movie) => console.log(movie.title));
-  //   console.log(titles);
-  // const [movie, setMovie] = useState()
-  // const movie = state.list
-  // const selected = state.selected
   return (
     <div>
       <h1>Redux Movies</h1>
