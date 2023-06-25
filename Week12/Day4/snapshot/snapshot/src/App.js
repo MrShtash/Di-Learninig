abecdf951c40820f42125562993c3b0677370194
Stanislav <bassketb86@gmail.com>
2023-06-25 09:29:21 +0300

    f

--- Week12/Day4/snapshot/snapshot/src/App.js
+++ Week12/Day4/snapshot/snapshot/src/App.js
@@ -1,13 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import SnapShot from './components/SnapShot'
+
+// const apiKey = `6yhduMvRFSikO72L0csBkfZR5JdqWW7pAngVjpQaJuo3Eu26nEQfOwYi`
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        
-      </header>
+    <div className="pp">
+      <SnapShot/>
     </div>
   );
 }
---
+++ Week12/Day4/snapshot/snapshot/src/components/SnapShot.js
@@ -0,0 +1,42 @@
+import './SnapShot.css';
+
+function SnapShot() {
+    return(
+        <div>
+            <div className='title'>
+                <h1>SnapShot</h1>
+            </div>
+            <form action='' className='search-form'>
+                <input type='text' placeholder='Search...'/>
+                <button type='submit'
+                        value=''
+                        // disabled
+                        >
+                            <svg height='32' width='32'>
+                                <path
+                                    d="M19.427 21.427a8.5 8.5 0 1 1 2-2l5.585 5.585c.55.55.546 1.43 0 1.976l-.024.024a1.399 1.399 0 0 1-1.976 0l-5.585-5.585zM14.5 21a6.5 6.5 0 1 0 0-13 6.5 6.5 0 0 0 0 13z"
+                                    fill="#ffffff"
+                                    fillRule="evenodd"
+                                />
+                            </svg>
+                        </button>
+            </form>
+            <nav>
+                <ul>
+                    <li>Mountain</li>
+                    <li>Beaches</li>
+                    <li>Birds</li>
+                    <li>Food</li>
+                </ul>
+            </nav>
+            <h2>Pictures</h2>
+            <div className='container'>
+                <div>
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default SnapShot
