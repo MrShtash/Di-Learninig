5c1f12b19903a91cda77499bedaab4cdbcb2c160
Stanislav <bassketb86@gmail.com>
2023-06-26 13:12:04 +0300

    add proxy

--- Final Project/backend/server.js
+++ Final Project/backend/server.js
@@ -4,13 +4,15 @@ const dotenv = require('dotenv');
 // const users_router = require('./routes/rss.js');
 
 const app = express();
+app.use(cors());
 dotenv.config();
 
-app.get('/', (req, res) => {
+app.get('/api/data', (req, res) => {
     const data = {
-        msg: 'Hello, this is Scriptum'
+        message: 'Hello, this is Scriptum'
     };
     res.json(data)
+    console.log(data);
 })
 
 app.listen(process.env.PORT || 3000, () => {
@@ -19,7 +21,6 @@ app.listen(process.env.PORT || 3000, () => {
 
 // app.set('view engine' , 'ejs');
 app.use(express.urlencoded({extended:true}));
-app.use(express.static(__dirname + '/public'));
-app.use(cors());
+// app.use(express.static(__dirname + '/public'));
 // app.use(express.json());
 // app.use(users_router.router)
---
+++ Final Project/frontend/scriptum_app/src/components/Scriptum.js
@@ -0,0 +1,23 @@
+import React, {useEffect, useState} from 'react';
+
+function Scriptum() {
+    const [message, setMessage] = useState('');
+    useEffect(() => {
+        fetch(`/api/data`)
+        .then(response => {console.log(response)
+            return response.json()})
+        .then(data => {console.log(data)
+            setMessage(data.message)
+        })
+        .catch(error => console.log(error));
+        
+    }, [])
+
+    return(
+        <div>
+            <h1>{message}</h1>
+        </div>
+    )
+}
+
+export default Scriptum
