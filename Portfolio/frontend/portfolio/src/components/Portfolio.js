23de9529b6c1131fe76efb62a7eac3fde3352b3e
Stanislav <bassketb86@gmail.com>
2023-07-09 14:38:54 +0300

    porfolio

--- Portfolio/frontend/portfolio/src/components/Portfolio.js
+++ Portfolio/frontend/portfolio/src/components/Portfolio.js
@@ -12,7 +12,10 @@ import {
         faCss3Alt,
         faHtml5
     } from '@fortawesome/free-brands-svg-icons';
-import icondjango from '../static/icondjango.jpeg'
+import icondjango from '../static/icondjango.jpeg';
+import ovi from '../static/ovi.png';
+import ca from '../static/ca.png';
+import code from '../static/5764018.jpg'
 
 import './Portfolio.css'
 
@@ -23,11 +26,21 @@ const Portfolio = () => {
         <h1>ST</h1>
         <nav>
           <ul>
-            <li><a href="#about" style={{color: 'blue', fontsize: 'bold'}}>About Me</a></li>
-            <li><a href="#skills" style={{color: 'blue', fontsize: 'bold'}}>Skills</a></li>
-            <li><a href="#education" style={{color: 'blue', fontsize: 'bold'}}>Education</a></li>
-            <li><a href="#projects" style={{color: 'blue', fontsize: 'bold'}}>Projects</a></li>
-            <li><a href="#contacts" style={{color: 'blue', fontsize: 'bold'}}>Contacts</a></li>
+            <li><a href="#about"
+                    style={{color: 'blue',
+                    fontsize: 'bold'}}>About Me</a></li>
+            <li><a href="#skills"
+                    style={{color: 'blue',
+                    fontsize: 'bold'}}>Skills</a></li>
+            <li><a href="#education"
+                    style={{color: 'blue',
+                    fontsize: 'bold'}}>Education</a></li>
+            <li><a href="#projects"
+                    style={{color: 'blue',
+                    fontsize: 'bold'}}>Projects</a></li>
+            <li><a href="#contacts"
+                    style={{color: 'blue',
+                    fontsize: 'bold'}}>Contacts</a></li>
           </ul>
         </nav>
       </header>
@@ -35,69 +48,68 @@ const Portfolio = () => {
       <div id="about" className="portfolio-section">
         <div className="about-content">
             <div className="about-image">
-            <img src="../static/avatar.jpeg" alt="" />
+                <img src="../static/avatar.jpeg" alt="" />
             </div>
             <div className="about-text">
-            <h2>Stanislav Tuf</h2><br></br>
-            <h3>Development and design</h3><br></br>
-            <p>I believe that any product can be developed and improved</p>
-            <a href="#projects" style={{color: 'blue', fontsize: 'bold'}}>See More</a>
+                <h2>Stanislav Tuf</h2><br></br>
+                    <h3>Development and design</h3><br></br>
+                        <p>I believe that any product can be developed and improved</p>
+                            <a href="#projects" style={{color: 'blue', fontsize: 'bold'}}>See More</a>
             </div>
         </div>
+
+
        </div>
+            <div className="download-cv">
+                <h2 className="text-center">Download CV</h2><br></br>
+                <a href="../static/TUFS.pdf" download>
+                    Get CV
+                </a>
+            </div>
       <div id="skills" className="portfolio-section">
-        <h2>Skills</h2>
+        <h2 className="text-center">Skills</h2>
         <div className="skills-grid">
           <div className="skill">
-            {/* <img src="html_icon.png" alt="HTML" /> */}
-            <FontAwesomeIcon icon={faHtml5} style={{width: '60px', height: '60px'}}/>
-            {/* <span>HTML</span> */}
+            <FontAwesomeIcon icon={faHtml5}
+                    style={{width: '60px', height: '60px'}}/>
           </div>
           <div className="skill">
-            {/* <img src="css_icon.png" alt="CSS" /> */}
-            <FontAwesomeIcon icon={faCss3Alt} style={{width: '60px', height: '60px'}}/>
-            {/* <span>CSS</span> */}
+            <FontAwesomeIcon icon={faCss3Alt}
+                    style={{width: '60px', height: '60px'}}/>
           </div>
           <div className="skill">
-            {/* <img src="react_icon.png" alt="React" /> */}
-            <FontAwesomeIcon icon={faReact} style={{width: '60px', height: '60px'}}/>
-            {/* <span>React</span> */}
+            <FontAwesomeIcon icon={faReact}
+                    style={{width: '60px', height: '60px'}}/>
           </div>
           <div className="skill">
-            {/* <img src="react_icon.png" alt="Node.js" /> */}
-            <FontAwesomeIcon icon={faNode} style={{width: '60px', height: '60px'}}/>
-            {/* <span>Node.js</span> */}
+            <FontAwesomeIcon icon={faNode}
+                    style={{width: '60px', height: '60px'}}/>
           </div>
           <div className="skill">
-            {/* <img src="../../public/static/icon-django.jpeg" alt="Django" /> */}
-            <img src={icondjango} alt="Django" style={{width: '60px', height: '60px'}}/>
-            {/* <span>Django</span> */}
+            <img src={icondjango} alt="Django"
+                    style={{width: '60px', height: '60px'}}/>
           </div>
           <div className="skill">
-            {/* <img src="react_icon.png" alt="Python" /> */}
-            <FontAwesomeIcon icon={faPython} style={{width: '60px', height: '60px'}}/>
-            {/* <span>Python</span> */}
+            <FontAwesomeIcon icon={faPython}
+                    style={{width: '60px', height: '60px'}}/>
           </div>
           <div className="skill">
-            {/* <img src="react_icon.png" alt="Figma" /> */}
-            <FontAwesomeIcon icon={faFigma} style={{width: '60px', height: '60px'}}/>
-            {/* <span>Figma</span> */}
+            <FontAwesomeIcon icon={faFigma}
+                    style={{width: '60px', height: '60px'}}/>
           </div>
           <div className="skill">
-            {/* <img src="./static/database-solid.svg" alt="PostgreSQL" /> */}
-            <FontAwesomeIcon icon={faDatabase} style={{width: '60px', height: '60px'}}/>
-            {/* <span>PostgreSQL</span> */}
+            <FontAwesomeIcon icon={faDatabase}
+                    style={{width: '60px', height: '60px'}}/>
           </div>
           <div className="skill">
-            {/* <img src="./static/database-solid.svg" alt="GitHub" /> */}
-            <FontAwesomeIcon icon={faGithub} style={{width: '60px', height: '60px'}}/>
-            {/* <span>GitHub</span> */}
+            <FontAwesomeIcon icon={faGithub}
+                    style={{width: '60px', height: '60px'}}/>
           </div>
         </div>
       </div>
 
       <div id="education" className="portfolio-section">
-        <h2>Education</h2>
+        <h2 className="text-center">Education</h2>
         <div className="education-logos">
           <img src="./static/di.png" alt="" />
           <img src="./static/gb.jpeg" alt="" />
@@ -105,20 +117,82 @@ const Portfolio = () => {
       </div>
 
       <div id="projects" className="portfolio-section">
-        <h2>Projects</h2>
+        <h2 className="text-center">Projects</h2>
+        <div className="project-slider">
+          <div className="project">
+            <div className="project-image">
+              <img src={ovi} alt="OVI" style={{width: '300px', height: '100%'}}/>
+              <div className="project-hover">
+                <h3>Alex Ovechkin Tribute Page</h3>
+                <p>Bootcamp Prep Project</p>
+                <a href="https://github.com/MrShtash/DI-Bootcamp-Stage1/tree/main/Final-Project"
+                        target="_blank"
+                        rel="noopener noreferrer">
+                  Learn More
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="project">
+            <div className="project-image">
+              <img src={ca} alt="CA" style={{width: '300px', height: '100%'}}/>
+              <div className="project-hover">
+                <h3>Comfortalia</h3>
+                <p>Servis for Olim's</p>
+                <a href="https://github.com/MrShtash/DI_Hackathon_1"
+                        target="_blank"
+                        rel="noopener noreferrer">
+                  Learn More
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="project">
+            <div className="project-image">
+              <img src={code} alt="" style={{width: '300px', height: '100%'}}/>
+              <div className="project-hover">
+                <h3>See Soon</h3>
+                <p>See Soon</p>
+                <a href="https://github.com/MrShtash/DI_Hackathon_1"
+                        target="_blank"
+                        rel="noopener noreferrer">
+                  Learn More
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
 
       <div id="contacts" className="portfolio-section">
-        <h2>Contacts</h2>
+        <h2 className="text-center">Contacts</h2>
         <div className="contact-form">
           <h3>Get in touch</h3>
           <input type="text" placeholder="Name" />
-          <input type="email" placeholder="Email" />
-          <textarea placeholder="Message"></textarea>
+          <input type="email" placeholder="Example@email.com" />
+          <textarea placeholder="Your message"></textarea>
+          <button type="submit" style={{backgroundColor: 'white',
+                                        color: 'blue',
+                                        borderColor: 'white',
+                                        borderRadius: '5px',
+                                        cursor: 'pointer'
+                                    }}>Send</button>
           <div className="social-icons">
-            <a href="https://www.linkedin.com/in/stuf/"><FontAwesomeIcon icon={faLinkedin} style={{width: '30px', height: '30px'}}/></a>
-            <a href="https://github.com/MrShtash"><FontAwesomeIcon icon={faGithub} style={{width: '30px', height: '30px'}}/></a>
-            <a href="https://www.facebook.com/StanislavTuf?locale=ru_RU"><FontAwesomeIcon icon={faFacebook} style={{width: '30px', height: '30px'}}/></a>
+            <a href="https://www.linkedin.com/in/stuf/"
+                    target="_blank"
+                    rel="noopener noreferrer"><FontAwesomeIcon icon={faLinkedin}
+                                                style={{width: '30px',
+                                                height: '30px'}}/></a>
+            <a href="https://github.com/MrShtash"
+                    target="_blank"
+                    rel="noopener noreferrer"><FontAwesomeIcon icon={faGithub}
+                                                style={{width: '30px',
+                                                height: '30px'}}/></a>
+            <a href="https://www.facebook.com/StanislavTuf?locale=ru_RU"
+                    target="_blank"
+                    rel="noopener noreferrer"><FontAwesomeIcon icon={faFacebook}
+                                                style={{width: '30px',
+                                                height: '30px'}}/></a>
           </div>
         </div>
       </div>
