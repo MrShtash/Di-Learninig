eb5ea9135b064dafeffc0a60a217cc697b22a002
Stanislav <bassketb86@gmail.com>
2023-08-02 18:13:29 +0300

    !

--- Computational Complexity & Big O Notation/logn.py
+++ Computational Complexity & Big O Notation/logn.py
@@ -1,23 +1,36 @@
 # condition 1 - sorted array / list
 # condition 2 - value to compare to
 
-sorted_array = list(range(1000))
-value_to_search = 2000
+# sorted_array = list(range(1000))
+# value_to_search = 1500
 
-right = len(sorted_array)
-left = 0
-middle = sorted_array[len(sorted_array) // 2]
+# right = len(sorted_array)
+# left = 0
+# middle = sorted_array[len(sorted_array) // 2]
 # print(middle)
 
-if middle == value_to_search:
-    print(f"FOUND SEARCH VALUE - {value_to_search}!")
-elif middle < value_to_search:
-    left = middle
-    middle = (left + right) // 2
-elif middle > value_to_search:
-    right = middle
-    middle = (left + right) // 2
-else:
-    print('NO SUCH VALUE')
+def binary_search(array, value_to_search):
+    middle = array[len(array) // 2]
+    right = len(array)
+    left = 0
 
-print()
+    while True:
+        if middle == value_to_search:
+            print(f"FOUND SEARCH VALUE - {value_to_search}!")
+            break
+
+        elif middle < value_to_search:
+            left = middle
+            middle = (left + right) // 2
+        elif middle > value_to_search:
+            right = middle
+            middle = (left + right) // 2
+        else:
+            print('NO SUCH VALUE')
+            break
+
+# print()
+some_array = list(range(1000))
+value = 50
+
+binary_search(some_array, value)
